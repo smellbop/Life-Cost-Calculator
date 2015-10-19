a679c5310cc71cdcfae1e5bf31464dcaebd86c95
Elliot Pollard <mail@elliotpollard.co.uk>
2015-10-19 22:58:26 +0100

    Implemented home button on settings screen

--- app/src/main/java/com/tigerprawnindustries/money/lifecostcalculator/SettingsActivity.java
+++ app/src/main/java/com/tigerprawnindustries/money/lifecostcalculator/SettingsActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
@@ -178,13 +179,19 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
                 return true;
             }
             return super.onOptionsItemSelected(item);
         }
     }
 
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
